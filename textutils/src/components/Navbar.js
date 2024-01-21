e17922a1ae18a433e2d62442eb96556f744ca33a
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-01-21 15:43:37 +0530

    Enhanced text processing: Implemented support for uppercase and lowercase conversions, and added word count functionality.

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           { props.title }
@@ -38,7 +38,7 @@ export default function Navbar(props) {
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
+            <button className="btn btn-primary" type="submit">
               Search
             </button>
           </form>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -2,28 +2,46 @@ import React, { useState } from 'react';
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log("Uppercase was Clicked");
-        setText("you have clicked on handUpClick")
+        // console.log("Uppercase was Clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText)
     }
-    const handleOnChange = ()=>{
-        console.log("on change");
+
+    const handleLoClick = ()=>{
+      // console.log("Uppercase was Clicked" + text);
+      let newText = text.toLowerCase();
+      setText(newText)
+  }
+
+    const handleOnChange = (event)=>{
+        //console.log("on change");
+        setText(event.target.value)
     }
-    const [text, setText] = useState('Enter the text');
+
+    const [text, setText] = useState('');
     //setText('new value');
   return (
     <>
-        <div class="mb-3">
-
-           
+        <div className="container">           
            <h3>{ props.heading }</h3>
             <textarea
             className="form-control" value={text} onChange={ handleOnChange }
             id="exampleFormControlTextarea1"
             rows="8"
             ></textarea>
-            <button className="btn btn-primary my-3" onClick={
+            <button className="btn btn-primary my-3 mx-2 " onClick={
                handleUpClick 
             }>Convert To Uppercase</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={
+               handleLoClick 
+            }>Convert To Lowercase</button>
+        </div>
+        <div className='container my-3'>
+          <h2>Your text Summary</h2>
+          <p>{text.split(' ').length} words and {text.length} characters</p>
+          <p>{0.008 * text.split(" ").length} Minutes read</p>
+          <h2>Preview</h2>
+          <p>{text}</p>
         </div>
     </>
   );
