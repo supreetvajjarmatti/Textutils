07d8e2459d1a226b31064ce83f80cfa2867b0d98
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-01-21 16:00:47 +0530

    Add 'About Us' section

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
    
 <Navbar title='TextUtils'/>
 <div className='container my-3'>
-<TextForm heading='Enter the Text'/>
+{/* <TextForm heading='Enter the Text'/> */}
+<About/>
 </div>
 
     </>
